cf2016e046878b5c68a931248b4f932fe224c6ab
unknown <miemail@dominio.com>
2021-11-27 09:58:58 -0500

    plantillas

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import './style/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Codepen from './components/Codepen'
+import Bootstrap from './components/Bootstrap'
+import Plantilla from './components/plantilla'
+
+
+
+const App = ()=>{
+    return(
+        // <Codepen />
+        //<Bootstrap />
+        <Plantilla />
+    )
+}
+
+export default App;
---
+++ src/components/Codepen.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Codepen =()=>{
+    return(
+        
+        <div className="wrapper fadeInDown">
+        <div id="formContent">
+          {/*<!-- Tabs Titles -->*/}
+          <h2 className="active"> Sign In </h2>
+          <h2 className="inactive underlineHover">Sign Up </h2>
+      
+          {/*<!-- Icon -->*/}
+          <div className="fadeIn first">
+            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+          </div>
+      
+          {/*<!-- Login Form -->*/}
+          <form>
+            <input type="text" id="login" className="fadeIn second" name="login" placeholder="login"/>
+            <input type="text" id="password" className="fadeIn third" name="login" placeholder="password"/>
+            <input type="submit" className="fadeIn fourth" value="Log In"/>
+          </form>
+      
+          {/*<!-- Remind Passowrd -->*/}
+          <div id="formFooter">
+            <a className="underlineHover" href="#">Forgot Password?</a>
+          </div>
+      
+        </div>
+      </div>
+    
+    )
+}
+
+export default Codepen;
